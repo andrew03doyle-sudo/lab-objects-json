86c0b4da116964aa7df13c5e6d1260da8c965c9b
andrew03doyle-sudo <andrew03doyle@gmail.com>
2025-11-24 15:03:49 -0500

    adding  part 2

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ console.log("=== Part 1: Student Object ===");
 console.log("Student name:", student.name);
 console.log("Student age:", student.age);
 console.log("Display info:", student.displayInfo());
+
+
+
+console.log("\n=== Part 2: JSON ===");
+
+const studentJSON = JSON.stringify(student);
+console.log("Student as JSON string:", studentJSON);
+
+const studentFromJSON = JSON.parse(studentJSON);
+console.log("Student object created from JSON:", studentFromJSON);
+
+console.log("Are the two objects strictly equal?", student === studentFromJSON);
